08f6f19ae2c5bc13e6a43eb3553a62a016a07706
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-11-11 20:30:22 -0500

    Test files changes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
-import env from '../src/config/envs';
-import {Server} from '../src/frameworks&InterfaceAdapaters&Presentation/express/server'
+import env from '../src/presentation/express/config/envs';
+import {Server} from '../src/presentation/express/server'
 
-vi.mock("../src/frameworks&InterfaceAdapaters&Presentation/express/server")
+vi.mock("../src/presentation/express/server")
 
 describe('Should call the server with parameters and start', ()=>{
     test('Should work', async()=>{
