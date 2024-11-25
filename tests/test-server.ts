72f5bed47239f8922f577e0f54032d37a1421b05
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-11-25 17:02:42 -0500

    feat: user testing

--- tests/test-server.ts
+++ tests/test-server.ts
@@ -1,9 +1,9 @@
 import { AppRoutes } from '../src/Presentation/express/routes/index.routes';
-import env from '../src/presentation/express/config/envs';
+// import env from '../src/presentation/express/config/envs';
 import { Server } from '../src/Presentation/express/server';
 
 // Crear servidor de prueba
 export const testserver = new Server({
-  port: env.PORT,
+  port: 0,
   routes: AppRoutes.routes
 });
