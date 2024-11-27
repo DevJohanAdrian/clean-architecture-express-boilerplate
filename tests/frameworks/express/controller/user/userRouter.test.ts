6b322192bbc8e295c3a4b48699d749844a64cbeb
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-11-26 23:50:41 -0500

    feat: dokerfile

--- tests/frameworks/express/controller/user/userRouter.test.ts
+++ tests/frameworks/express/controller/user/userRouter.test.ts
@@ -1,10 +1,9 @@
 import request from 'supertest';
 import { StatusCodes } from 'http-status-codes';
-
 import { testserver } from '../../../../test-server';
-// import type { ServiceResponse } from "../../../../../src/presentation/express/common/models/serviceResponse";
+import { ServiceResponse } from '../../../../../src/presentation/express/common/models/serviceResponse';
 
-describe('', () => {
+describe.skip('', () => {
   beforeAll(async () => {
     if (!testserver.isRunning()) {
       await testserver.start();
@@ -18,8 +17,21 @@ describe('', () => {
   });
 
   it('Should return all users on db and ok status code', async () => {
+    // Act
     const response = await request(testserver.app).get('/v1/api/users/');
-
+    const result: ServiceResponse = response.body;
+    console.log('testting', result);
+    // Assert
     expect(response.statusCode).toEqual(StatusCodes.OK);
+    expect(result[0].id).toEqual(expect.any(Number));
+    expect(result[0].name).toEqual(expect.any(String));
   });
+
+  // it("Should handle SQL injection attempts gracefully", async ()=>{
+  //   const { body } = await request(testserver.app).get('/api/v1/role/').query({ id: '1; DROP TABLE roles;' })
+
+  //    // Assert
+  //    expect(body.error).toBe(false)
+  //    expect(body.statusCode).toBe(200) // debe retornar un error
+  // })
 });
